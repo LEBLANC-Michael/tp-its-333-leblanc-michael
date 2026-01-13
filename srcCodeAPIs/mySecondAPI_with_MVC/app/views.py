92d48196978b89d79995058be5961b12d8d376cd
Michael Leblanc <rien>
2026-01-13 09:13:38 +0100

    Premier commit

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -4,6 +4,9 @@ from flask import render_template, request, jsonify
 ### EXO1 - simple API
 
 ### EXO2 - API with simple display
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 ### EXO3 - API with parameters display 
 
