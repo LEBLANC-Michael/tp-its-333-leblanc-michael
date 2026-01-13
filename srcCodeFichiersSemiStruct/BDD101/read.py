7cf6c4aa01bf6ca3171d54aa34be550e1fe98981
Michael Leblanc <rien>
2026-01-13 10:29:39 +0100

    deuxieme commit

--- srcCodeFichiersSemiStruct/BDD101/read.py
+++ srcCodeFichiersSemiStruct/BDD101/read.py
@@ -1,11 +1,29 @@
 import json
-f = open('data.json')
-data = json.load(f)
-f.close ()
 
-print(data)
-print(data["features"])
-print(data["features"][0]["geometry"])
+# 1. Lire le fichier
+with open("data.json", "r") as f:
+    data = json.load(f)
 
-for i in data["features"]:
-    print(i ["geometry"]["coordinates"][0])  
+# 2. Modifier les coordonnées du premier point
+data["features"][0]["geometry"]["coordinates"] = [50.0, 30.0]
+
+# 3. Créer une nouvelle feature (nouveau couple de coordonnées)
+new_feature = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [10.0, 5.0]
+    },
+    "properties": {
+        "prop0": "nouvelle valeur"
+    }
+}
+
+# 4. Ajouter la nouvelle feature à la liste
+data["features"].append(new_feature)
+
+# 5. Sauvegarder dans le fichier
+with open("data.json", "w") as f:
+    json.dump(data, f, indent=2)
+
+print("Coordonnées mises à jour et nouveau point ajouté !")
